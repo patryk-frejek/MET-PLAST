b7da1445b16ce858b2a0e0ac2a284bfbcee2a097
Patryk Frejek <freju13@gmail.com>
2023-01-30 09:31:03 +0100

    navbar upgrade, logochange etc

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 window.onload = init;
 const toggle = document.querySelector(".btn-menu");
 const navbar = document.querySelector(".navigation__list");
+const header = document.querySelector(".header");
 function init() {
 
 	toggle.onclick = () => {
 		navbar.classList.toggle("active");
+		header.classList.toggle("active");
 		console.log("testt");
 	};
+	
 }
 
 
