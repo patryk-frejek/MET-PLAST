0a92bf52ab556e395a32ec9d545c3c8f1a5d3942
Patryk Frejek <freju13@gmail.com>
2023-02-03 13:32:20 +0100

    dropmenu nav function

---
+++ script2.js
@@ -0,0 +1,20 @@
+const nav_item = document.querySelectorAll(".navigation__item");
+const dropmenu = document.querySelectorAll(".dropmenu");
+window.onload = init;
+function init() {
+	for (let i = 0; i < nav_item.length; i++) {
+		nav_item[i].onmouseenter = () => dropmenu[i].classList.add("active");
+		dropmenu[i].onmouseenter = () => dropmenu[i].classList.add("active");
+		dropmenu[i].onmouseleave = () => {
+			setTimeout(() => {
+				dropmenu[i].classList.remove("active");
+			}, 100);
+			
+		}
+		nav_item[i].onmouseleave = () =>
+			setTimeout(() => {
+				dropmenu[i].classList.remove("active");
+			}, 100);
+	}
+}
+w
