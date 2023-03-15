1eb48067adc4db450dd1f379345224813e45c40a
Patryk Frejek <freju13@gmail.com>
2023-03-15 07:00:24 +0100

    add animation for  offer document-

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-
 window.onload = init;
 const toggle = document.querySelector(".btn-menu");
 const navbar = document.querySelector(".navigation__list");
 const header = document.querySelector(".header");
 function init() {
-
 	toggle.onclick = () => {
 		navbar.classList.toggle("active");
 		header.classList.toggle("active");
-		console.log("testt");
 	};
-	
 }
 
+window.onscroll = checkBox;
+checkBox();
 
+function checkBox() {
+	const box = document.getElementsByClassName("box");
+	const activationPoint = (window.innerHeight / 5) * 4;
+	console.log(top);
+	for (let i = 0; i < box.length; i++) {
+		var top = box[i].getBoundingClientRect().top;
+		if (top < activationPoint) {
+			box[i].classList.remove("box");
+		}
+	}
+}
